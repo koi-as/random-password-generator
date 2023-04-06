75a5e9ebd0cbda66bd00fdd6c83030b2bd5384cb
Koi-AS <lightningblade.901@gmail.com>
2023-04-06 00:08:39 -0400

    Added comments to help guide my work in the JS file

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# random-password-generator
-The goal of this project is to create a random password generator that allows someone to generate an 8-129 character password using lowercase, uppercase, special, and numerical characters.
+# Random Password Generator
+
+## Description
+
+The goal of this project is to create a random password generator that allows someone to generate an 8-128 character password using lowercase, uppercase, special, and numerical characters.
+
+## Installation
+
+N/A
+
+## Usage
+
+
+
+## Credits
+
+
+
+## License
+
+N/A
--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword () {
+  console.log("Button press");
+  //1. prompt the user
+  //  a. select password length 8 < 128
+  //  b. select lowercase letters
+  //  c. select uppercase letters
+  //  d. select numbers
+  //  e. select special characters
+  //2. validate inputs
+  //3. display password
+
+  return "Password";
+}
+
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var password = generatePassword(); //calling the generatePassword function
+  var passwordText = document.querySelector("#password"); //This is where the generated password displays on the screen
 
-  passwordText.value = password;
+  passwordText.value = password; //plugs the generated password on the screen
 
 }
 
